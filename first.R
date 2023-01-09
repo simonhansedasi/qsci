6ad0895c07c7ba3d4cd946a88526b47c6b46f361
Simon-Hans Edasi <woses21@gmail.com>
2023-01-09 15:10:11 -0800

    adding stuff

---
+++ first.R
@@ -0,0 +1,9 @@
+# Section One ---------------------------------
+# here is a comment
+z <- 5
+x <- 7
+# Section Three ---------------------------------
+print(z - x)
+# Section Four ---------------------------------
+y <- 9
+print(y)
