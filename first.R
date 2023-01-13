69ce2bc639356132a784d3a16cd9320b060b6efb
Simon-Hans Edasi <woses21@gmail.com>
2023-01-13 15:42:17 -0800

    removing dead files

--- first.R
+++
@@ -1,9 +0,0 @@
-# Section One ---------------------------------
-# here is a comment
-z <- 5
-x <- 7
-# Section Three ---------------------------------
-print(z - x)
-# Section Four ---------------------------------
-y <- 9
-print(y)
